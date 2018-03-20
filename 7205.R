33218853dee3889228664ed09f8995cacfaa5b54
Yifei Wang <wykiwydiwyf@gmail.com>
2018-03-20 14:38:54 +1000

    finished?

--- 7205.R
+++ 7205.R
@@ -20,8 +20,7 @@ length(which(dataset$x1 < dataset$x2))
 #looks most likely that x1,x2 are random number between 0-100000
 
 
-#####################FUNCTION-1###############
-#Function creates index for R-tree
+###################FUNCTION-1##Function creates index for R-tree###############
 
 # if there is m branches in parent nodes
 #  we put b_max data points in each child node
@@ -36,50 +35,40 @@ generate_branch_index = function(m,dataset_table){
   return(branch_number)
 }
 
-#####################FUNCTION-2###############
-#Function creates R-tree
+
+###################FUNCTION-2##Function creates R-tree###########################
+
 generate_r_tree = function(m,dataset_table) {
   b_min = integer(0.4*m)
   b_max = m
-  dataset_mean_tree = dataset_table
   data_layer = 1
-  while(b_max^data_layer < nrow(dataset_mean_tree)){
-    dataset_mean_tree = data.frame(dataset_mean_tree, parent_layer = NA)
-    for(i in c(seq(1,nrow(dataset_mean_tree),b_max^(data_layer)),nrow(dataset_mean_tree))){
-      if(i+b_max^data_layer-1< nrow(dataset_mean_tree)){
-        child_nodes_min = min(dataset_mean_tree$x1[i],dataset_mean_tree$x2[i])
-        child_nodes_max = max(dataset_mean_tree$x1[i+b_max^data_layer-1],dataset_mean_tree$x2[i+b_max^data_layer-1])
-        for(n in seq(i,i+b_max^data_layer-1,1)){
-          dataset_mean_tree[n,ncol(dataset_mean_tree)] = paste(child_nodes_min,child_nodes_max)
-        }
-      }else{
-        child_nodes_min = min(dataset_mean_tree$x1[i],dataset_mean_tree$x2[i])
-        child_nodes_max = max(dataset_mean_tree$x1[nrow(dataset_mean_tree)],dataset_mean_tree$x2[nrow(dataset_mean_tree)])
-        for(n in seq(i,nrow(dataset_mean_tree),1)){
-          dataset_mean_tree[n,ncol(dataset_mean_tree)] = paste(child_nodes_min,child_nodes_max)
-        }
+  while(b_max^data_layer < nrow(dataset_table)){
+    dataset_table = data.frame(dataset_table, parent_layer = NA)
+    for(i in seq(1,nrow(dataset_table),b_max^(data_layer))){
+      child_nodes_min = min(dataset_table[i:min((i+(b_max^data_layer)),nrow(dataset_table)),2],dataset_table[i:min((i+(b_max^data_layer)),nrow(dataset_table)),3])
+      child_nodes_max = max(dataset_table[i:min((i+(b_max^data_layer)-1),nrow(dataset_table)),2],dataset_table[i:min((i+(b_max^data_layer)-1),nrow(dataset_table)),3])
+      for(n in seq(i,min((i+b_max^data_layer),nrow(dataset_table)),1)){
+        dataset_table[n,ncol(dataset_table)] = paste(child_nodes_min,child_nodes_max)
       }
     }
     data_layer = data_layer + 1
   }
-  dataset_mean_tree = data.frame(dataset_mean_tree, parent_layer1 = NA)
-  child_nodes_min = min(dataset_mean_tree$x1[1],dataset_mean_tree$x2[1])
-  child_nodes_max = max(dataset_mean_tree$x1[nrow(dataset_mean_tree)],dataset_mean_tree$x2[nrow(dataset_mean_tree)])
-  for(n in seq(1,nrow(dataset_mean_tree),1)){
-    dataset_mean_tree[n,ncol(dataset_mean_tree)] = paste(child_nodes_min,child_nodes_max)
+  dataset_table = data.frame(dataset_table, parent_layer1 = NA)
+  child_nodes_min = min(dataset_table[1,1],dataset_table[1,2])
+  child_nodes_max = max(dataset_table[nrow(dataset_table),1],dataset_table[nrow(dataset_table),2])
+  for(n in seq(1,nrow(dataset_table),1)){
+    dataset_table[n,ncol(dataset_table)] = paste(child_nodes_min,child_nodes_max)
   }
-  return(dataset_mean_tree)
+  return(dataset_table)
 }
 
-#####################FUNCTION-3###############
-#A sub-function used in r-tree query function
+###################FUNCTION-3##A sub-function used in r-tree query function##################
 break_node = function(node){
   return(c(as.numeric(unlist(strsplit(unlist(node),split=" "))[1]),as.numeric(unlist(strsplit(unlist(node),split=" "))[2])))
 }
 
-#####################FUNCTION-4###############
-#Function query Rtree
-rtree_query = function(mvalue,dataset_table1,query_number){
+###################FUNCTION-4##Function query Rtree###################
+rtree_query = function(mvalue,dataset_table1){
   # There is only one root in rtree, so we start it from branches after root.
   node_access = paste(c(1,nrow(dataset_table1)),collapse = " ")
   
@@ -104,25 +93,27 @@ rtree_query = function(mvalue,dataset_table1,query_number){
     temp =2
     temp1 = 1
     # layer above child nodes
-    for (i in (ncol(dataset_table1)-2):5){
-      for (n in temp:nrow(node_access)){
-        for (k in seq(break_node(node_access[n])[1],break_node(node_access[n])[2],mvalue^(i-4))){
-          if (max(query_list$x1[j],query_list$x2[j])<=max(break_node(dataset_table1[k,i])[1],break_node(dataset_table1[k,i])[2]) && min(query_list$x1[j],query_list$x2[j])>=min(break_node(dataset_table1[k,i])[1],break_node(dataset_table1[k,i])[2])){
-            node_access = rbind(node_access,paste(c(k,min(nrow(dataset_table1),k+mvalue^(i-4)-1)),collapse = " "))
-          }
-          else if (max(query_list$x1[j],query_list$x2[j])>=max(break_node(dataset_table1[k,i])[1],break_node(dataset_table1[k,i])[2]) && min(query_list$x1[j],query_list$x2[j])<=max(break_node(dataset_table1[k,i])[1],break_node(dataset_table1[k,i])[2])){
-            node_access = rbind(node_access,paste(c(k,min(nrow(dataset_table1),k+mvalue^(i-4)-1)),collapse = " "))
-          }
-          else if (max(query_list$x1[j],query_list$x2[j])>=min(break_node(dataset_table1[k,i])[1],break_node(dataset_table1[k,i])[2]) && min(query_list$x1[j],query_list$x2[j])<=min(break_node(dataset_table1[k,i])[1],break_node(dataset_table1[k,i])[2])){
-            node_access = rbind(node_access,paste(c(k,min(nrow(dataset_table1),k+mvalue^(i-4)-1)),collapse = " "))
-          }
-          else if (max(query_list$x1[j],query_list$x2[j])>=max(break_node(dataset_table1[k,i])[1],break_node(dataset_table1[k,i])[2]) && min(query_list$x1[j],query_list$x2[j])<=min(break_node(dataset_table1[k,i])[1],break_node(dataset_table1[k,i])[2])){
-            node_access = rbind(node_access,paste(c(k,min(nrow(dataset_table1),k+mvalue^(i-4)-1)),collapse = " "))
+    if (ncol(dataset_table1)>6){
+      for (i in (ncol(dataset_table1)-2):5){
+        for (n in temp:nrow(node_access)){
+          for (k in seq(break_node(node_access[n])[1],break_node(node_access[n])[2],mvalue^(i-4))){
+            if (max(query_list$x1[j],query_list$x2[j])<=max(break_node(dataset_table1[k,i])[1],break_node(dataset_table1[k,i])[2]) && min(query_list$x1[j],query_list$x2[j])>=min(break_node(dataset_table1[k,i])[1],break_node(dataset_table1[k,i])[2])){
+              node_access = rbind(node_access,paste(c(k,min(nrow(dataset_table1),k+mvalue^(i-4)-1)),collapse = " "))
+            }
+            else if (max(query_list$x1[j],query_list$x2[j])>=max(break_node(dataset_table1[k,i])[1],break_node(dataset_table1[k,i])[2]) && min(query_list$x1[j],query_list$x2[j])<=max(break_node(dataset_table1[k,i])[1],break_node(dataset_table1[k,i])[2])){
+              node_access = rbind(node_access,paste(c(k,min(nrow(dataset_table1),k+mvalue^(i-4)-1)),collapse = " "))
+            }
+            else if (max(query_list$x1[j],query_list$x2[j])>=min(break_node(dataset_table1[k,i])[1],break_node(dataset_table1[k,i])[2]) && min(query_list$x1[j],query_list$x2[j])<=min(break_node(dataset_table1[k,i])[1],break_node(dataset_table1[k,i])[2])){
+              node_access = rbind(node_access,paste(c(k,min(nrow(dataset_table1),k+mvalue^(i-4)-1)),collapse = " "))
+            }
+            else if (max(query_list$x1[j],query_list$x2[j])>=max(break_node(dataset_table1[k,i])[1],break_node(dataset_table1[k,i])[2]) && min(query_list$x1[j],query_list$x2[j])<=min(break_node(dataset_table1[k,i])[1],break_node(dataset_table1[k,i])[2])){
+              node_access = rbind(node_access,paste(c(k,min(nrow(dataset_table1),k+mvalue^(i-4)-1)),collapse = " "))
+            }
           }
         }
-      }
-      temp = n+1
-    } 
+        temp = n+1
+      } 
+    }
     # child nodes
     for (n in temp:nrow(node_access)){
       for (k in seq(break_node(node_access[n])[1],break_node(node_access[n])[2],1)){
@@ -142,8 +133,7 @@ rtree_query = function(mvalue,dataset_table1,query_number){
       temp = n+1
     }
     rtree_result = as.numeric(node_access[temp:length(node_access)])
-    temp_set = rtree_test[rtree_test$key[rtree_query_result],1:3]
-    rtree_result = temp_set = temp_set[order(temp_set$key),]
+
     return(rtree_result)
   }
 }
@@ -165,27 +155,13 @@ for(m_val in 1:nrow(dataset)){
   }
 }
 
-#choose random five m value(m<30) from v_list
+#choose random five m value(m<100) from v_list
 
-m_list_s = sample(m_list[which(m_list<30 & m_list>5)],1)
+m_list_s = sample(m_list[which(m_list<100 & m_list>5)],1)
 
 
 #generate a r_tree using different hypothesis
 
-
-#=================#hypothesis 1#RUN THIS TO GENERATE A RTREE BY ORDER OF MEAN=============================
-##hypothesis 1##: use mean of x1,x2 to order dataset is the most effecient R-tree:
-
-
-for (i in 1:nrow(dataset)) {
-  dataset$mean[i] = (dataset$x1[i]+dataset$x2[i])/2
-}
-
-dataset_mean = dataset[order(dataset$mean), ]
-
-rtree_test = generate_r_tree(m_list_s[1],dataset_mean)
-
-branch_index_test = generate_branch_index(m_list_s[1],dataset_mean)
 
 #=================#hypothesis 2#RUN THIS TO GENERATE A RTREE BY ORDER OF MIN=============================
 ##hypothesis 2##: use minimun of x1,x2 to order dataset is the most effecient R-tree:
@@ -210,12 +186,26 @@ for (i in 1:nrow(dataset)) {
   dataset$max[i] = max(dataset$x1[i],dataset$x2[i])
 }
 
-dataset_max = dataset[order(dataset$min), ]
+dataset_max = dataset[order(dataset$max), ]
 
 rtree_test = generate_r_tree(m_list_s[1],dataset_max)
 
 branch_index_test = generate_branch_index(m_list_s[1],dataset_max)
 
+
+#=================#hypothesis 1#RUN THIS TO GENERATE A RTREE BY ORDER OF MEAN=============================
+##hypothesis 1##: use mean of x1,x2 to order dataset is the most effecient R-tree:
+
+
+for (i in 1:nrow(dataset)) {
+  dataset$mean[i] = (dataset$x1[i]+dataset$x2[i])/2
+}
+
+dataset_mean = dataset[order(dataset$mean), ]
+
+rtree_test = generate_r_tree(m_list_s[1],dataset_mean)
+
+branch_index_test = generate_branch_index(m_list_s[1],dataset_mean)
 
 #======================================[QUERY WITHOUT R-TREE]=============================================
 # [QUERY WITHOUT R-TREE]
@@ -243,9 +233,8 @@ time_of_query = system.time(
       }
     }
   }
-  query_result = query_result[which(complete.cases(query_result)),]
 )
-
+query_result = query_result[which(complete.cases(query_result)),]
 cat("The query time for" , query_time, "query/queries is", time_of_query[3],"seconds")
 
 #======================================[QUERY WITH R-TREE]================================================
@@ -259,14 +248,13 @@ cat("The query time for" , query_time, "query/queries is", time_of_query[3],"sec
 #Let's time the rtree query:
 
 
-time_of_rtree_query = system.time(
-  for (i in 1){
-    rtree_query_result = rtree_query(m_list_s[i],rtree_test)
-  }
-)
+
+time_of_rtree_query = system.time( for(i in 1){rtree_query_result = rtree_query(m_list_s[i],rtree_test)})
 
 cat("The query time for" , query_time, "query/queries is", time_of_rtree_query[3],"seconds")
 
+temp_set = rtree_test[rtree_query_result,(1:3)]
+rtree_result = temp_set[order(temp_set$key),]
 
 
 #=======================THE END=========================
